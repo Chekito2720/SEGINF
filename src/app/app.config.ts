9aa24687ac8b7d9cf217692e8570d4d6f052a416
Chekito2720 <2023371075@uteq.edu.mx>
2026-02-20 19:33:33 -0600

    Ejercicio1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+        preset: Lara
+      }
+    })
   ]
 };
